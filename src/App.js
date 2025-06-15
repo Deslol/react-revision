9cdf6fe5f5d5ebf8e2c74a5a6d60e35b992a1744
Cyril Chan <kwunming0911@gmail.com>
2025-06-15 22:28:42 +0100

    useState hook simple revise

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [advice, setAdvice] = useState("");
+
+    const getAdvice = async () => {
+        const res = await fetch("https://api.adviceslip.com/advice");
+        const {slip} = await res.json()
+        const {advice} = slip
+        setAdvice(advice)
+    }
+    return (
+        <div>
+            <h1>{advice}</h1>
+            <button onClick={getAdvice}>Get advice</button>
+        </div>
+    );
 }
 
 export default App;
