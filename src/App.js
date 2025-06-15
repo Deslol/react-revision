105d9e768e3bd9e290677dd8c6d001e4cdeca87b
Cyril Chan <kwunming0911@gmail.com>
2025-06-15 22:33:50 +0100

    Setting logic

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ import {useState} from "react";
 
 function App() {
     const [advice, setAdvice] = useState("");
+    const [count, setCount] = useState(0);
 
     const getAdvice = async () => {
         const res = await fetch("https://api.adviceslip.com/advice");
         const {slip} = await res.json()
         const {advice} = slip
         setAdvice(advice)
+        setCount((c) => c += 1)
     }
     return (
         <div>
-            <h1>{advice}</h1>
+            <h1>{advice || 'Click The Button Below To Get Advice.'}</h1>
             <button onClick={getAdvice}>Get advice</button>
+            <p>You have read <strong>{count}</strong> piece{count > 1 ? '' : 's'} of advice</p>
         </div>
     );
 }
